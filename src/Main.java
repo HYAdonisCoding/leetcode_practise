5700404619752dec392e3908c09bc00d7faf65ca
Adam <296786475@qq.com>
2022-05-28 15:54:46 +0800

    Three Java schemes for traversing list

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] list = {1, 3, 4, 6, 7, 4, 9, 10};
 
+		// for循环
+		for (int i = 0; i < list.length; i++) {
+			System.out.println("1 -" + list[i]);
+		}
+		
+		// for earch
+		for (Integer i: list) {
+			System.out.println("2 -" + i);
+		}
+
+
+		// 迭代器
+		Iterator<Integer> iterator = Arrays.stream(list).iterator();
+		while (iterator.hasNext()) {
+			Integer i = iterator.next();
+			System.out.println("3 -" + i);
+		}
 	}
 
 }
